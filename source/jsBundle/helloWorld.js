387d50ef35d2499694afc94674fc7bc5c1032943
Jeff Schwartz <jefftschwartz@gmail.com>
2021-05-06 14:12:24 -0400

    now dynamically adding styled text to the page

--- source/jsBundle/helloWorld.js
+++ source/jsBundle/helloWorld.js
@@ -1,1 +1,7 @@
-export default () => console.log("Hello, world!");
+// export default () => console.log("Hello, world!");
+export default () => {
+    const para = document.createElement("p");
+    para.append("This text was dynamically added to the page so if you see this paragraph then JavaScript bundling has worked!");
+    para.setAttribute("class", "copy");
+    document.getElementsByTagName("main")[0].append(para);
+}
